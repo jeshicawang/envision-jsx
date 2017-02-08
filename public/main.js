7c239d57585c923db8880593a2ab8fe44dba6541
jeshicawang <jeshicawang@gmail.com>
2017-02-08 15:40:17 -0800

    User can view a tidy tree. #2

--- public/main.js
+++ public/main.js
@@ -1,6 +1,56 @@
+const d3 = require('d3');
+
 const rootFile = '../mytrips/src/components/app.js';
 
 fetch('/tree?root=' + rootFile)
   .then(results => results.json())
-  .then(json => console.log(json))
+  .then(drawTree)
   .catch(error => console.error(error));
+
+function drawTree(data) {
+
+  const root = d3.hierarchy(data, d => d.children ? d.children : null);
+
+  const width = 960;
+  const height = 960;
+
+  const svg = d3.select('#tree')
+      .attr('width', width)
+      .attr('height', height)
+
+  const g = svg.append('g')
+      .attr('transform', 'translate(80,80)')
+
+  const tree = d3.tree()
+    .size([width - 160, height - 160]);
+
+  const link = g.selectAll('.link')
+    .data(tree(root).descendants().slice(1))
+    .enter().append('path')
+      .attr('class', 'link')
+      .attr('d', d => {
+        return "M" + d.x + "," + d.y
+          + "C" + d.x + "," + (d.y + d.parent.y) / 2
+          + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
+          + " " + d.parent.x + "," + d.parent.y;
+        });
+
+  const node = g.selectAll('.node')
+    .data(root.descendants())
+    .enter().append('g')
+      .attr('class', d => 'node' + (d.children ? ' node--internal' : ' node--leaf'))
+      .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')')
+
+  node.append('circle')
+    .attr('r', 2.5);
+
+  node.append('text')
+    .attr('dy', 3)
+    .attr('y', d => d.children ? -10 : 10)
+    .style('text-anchor', 'middle')
+    .text(d => {
+      console.log(d);
+      return d.data.name;
+    })
+
+}
