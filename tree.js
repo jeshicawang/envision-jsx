c6b2bf005486ce0f1c70dae1e5332b82f03680cf
jeshicawang <jeshicawang@gmail.com>
2017-02-09 01:03:06 -0800

    Reading data from AST into a hierarchy array. #3

--- tree.js
+++ tree.js
@@ -2,28 +2,54 @@ const fs = require('fs');
 const acorn = require('acorn-jsx');
 const walk = require('acorn/dist/walk');
 
+let rootDirectory;
+const hierarchy = [];
+
 // custom base adding functionality for walk to traverse JSX
 const base = Object.assign({}, walk.base, {
   JSXElement: (node, st, c) => node.children.forEach(n => c(n, st)),
   JSXExpressionContainer: (node, st, c) => c(node.expression, st)
 })
 
-const visitors = {
-  JSXElement: (node) => console.log(node.type),
-  JSXExpressionContainer: (node) => console.log(node.type)
-}
+const createIdentifierVisitors = (chain) => ({
+  VariableDeclarator: (node, state) => {
+    if (node.id.name !== state) return;
+    if (node.init.type === 'ArrowFunctionExpression') return;
+    const value = node.init.arguments[0].value;
+    const file = rootDirectory + value.substring(2);
+    rootDirectory = file.substring(0, file.lastIndexOf('/') + 1);
+    readFiles(file, { chain });
+  }
+})
 
-const readFiles = (rootFile) => {
+const createJSXElementVisitors = (ast) => ({
+  JSXElement: (node, { chain }, ancestors) => {
+    const componentChain = ancestors
+      .filter(a => a.type === 'JSXElement')
+      .map(a => a.openingElement.name.name)
+      .filter(name => name.charAt(0) === name.charAt(0).toUpperCase())
+      .reduce((hierarchy, name) => hierarchy + (hierarchy ? '.' : '') + name, chain);
+    if (!componentChain || componentChain === chain) return;
+    hierarchy.push(componentChain);
+    console.log(hierarchy);
+    if (!node.openingElement.selfClosing) return;
+    const state = node.openingElement.name.name;
+    walk.simple(ast, createIdentifierVisitors(componentChain), base, state)
+  }
+})
+
+const readFiles = (rootFile, state) => {
   fs.readFile(rootFile, 'utf-8', (err, data) => {
     if (err) return console.log(err);
     const ast = acorn.parse(data, { plugins: { jsx: true } });
-    walk.simple(ast, visitors, base);
+    walk.ancestor(ast, createJSXElementVisitors(ast), base, state);
   })
 }
 
 // rootFile is the file containing the ReactDOM.render() call.
 const getTree = (rootFile, callback) => {
-  readFiles(rootFile);
+  rootDirectory = rootFile.substring(0, rootFile.lastIndexOf('/') + 1);
+  readFiles(rootFile, { chain: '' });
 }
 
 module.exports = { getTree };
