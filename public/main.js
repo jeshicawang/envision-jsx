82dc463a9fc601d1184467350c2046eb386ec123
jeshicawang <jeshicawang@gmail.com>
2017-02-09 15:19:02 -0800

    Added update() function for rendering the tree. #4

--- public/main.js
+++ public/main.js
@@ -28,30 +28,39 @@ function drawTree(data) {
   const tree = d3.tree()
     .size([width - 160, height - 160]);
 
-  const link = g.selectAll('.link')
-    .data(tree(root).descendants().slice(1))
-    .enter().append('path')
-      .attr('class', 'link')
-      .attr('d', d => {
-        return "M" + d.x + "," + d.y
-          + "C" + d.x + "," + (d.y + d.parent.y) / 2
-          + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
-          + " " + d.parent.x + "," + d.parent.y;
-        });
+  update(root);
 
-  const node = g.selectAll('.node')
-    .data(root.descendants())
-    .enter().append('g')
-      .attr('class', d => 'node' + (d.children ? ' node--internal' : ' node--leaf'))
-      .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')')
+  function update(root) {
 
-  node.append('circle')
-    .attr('r', 2.5);
+    const link = g.selectAll('.link')
+      .data(tree(root).descendants().slice(1))
 
-  node.append('text')
-    .attr('dy', 3)
-    .attr('y', d => d.children ? -12 : 12)
-    .style('text-anchor', 'middle')
-    .text(d => d.id.substring(d.id.lastIndexOf('.') + 1));
+    link.exit().remove()
+
+    link.enter().append('path')
+        .attr('class', 'link')
+        .attr('d', d => {
+          return "M" + d.x + "," + d.y
+            + "C" + d.x + "," + (d.y + d.parent.y) / 2
+            + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
+            + " " + d.parent.x + "," + d.parent.y;
+          });
+
+    const node = g.selectAll('.node')
+      .data(root.descendants())
+      .enter().append('g')
+        .attr('class', d => 'node' + (d.children ? ' node--internal' : ' node--leaf'))
+        .attr('transform', d => 'translate(' + d.x + ',' + d.y + ')')
+
+    node.append('circle')
+      .attr('r', 2.5);
+
+    node.append('text')
+      .attr('dy', 3)
+      .attr('y', d => d.children ? -12 : 12)
+      .style('text-anchor', 'middle')
+      .text(d => d.id.substring(d.id.lastIndexOf('.') + 1));
+
+  }
 
 }
