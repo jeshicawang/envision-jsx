2e89c12f18430330717cfcf86c2786e4ca438d72
jeshicawang <jeshicawang@gmail.com>
2017-02-10 13:16:51 -0800

    Renaming argument variable. #6

--- bin/cmd.js
+++ bin/cmd.js
@@ -7,8 +7,8 @@ console.log('Envisioning...');
 
 program
   .version('1.0.0')
-  .arguments('<file>')
-  .action((file) => envision(file))
+  .arguments('<root-file>')
+  .action((rootFile) => envision(rootFile))
   .parse(process.argv);
 
 console.log('Done! Open index.html from envision/ to view rendered tree diagram.');
