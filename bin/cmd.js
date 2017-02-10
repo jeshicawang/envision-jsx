880fb388571e5a4b850b9bc2ea08fdb5ce5213b7
jeshicawang <jeshicawang@gmail.com>
2017-02-10 13:08:07 -0800

    Adding a command that takes a file as an argument and generates tree. #6

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
 
-const { setRootFile, writeTreeData } = require('../envision.js');
+const envision = require('../envision.js');
 const program = require('commander');
 
 console.log('Envisioning...');
 
 program
-  .version('1.0.3')
+  .version('1.0.0')
   .arguments('<file>')
   .action((file) => envision(file))
   .parse(process.argv);
 
-function envision(file) {
-  setRootFile(file);
-  writeTreeData();
-}
+console.log('Done! Open index.html from envision/ to view rendered tree diagram.');
