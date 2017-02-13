359fdcb38d072fd6c34deb8436fb978770c58c54
jeshicawang <jeshicawang@gmail.com>
2017-02-13 10:37:35 -0800

    Updating version in cmd.js. #11

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,7 +4,7 @@ const envision = require('../envision.js');
 const program = require('commander');
 
 program
-  .version('1.0.0')
+  .version('1.0.5')
   .arguments('<root-file>')
   .action((rootFile) => envision(rootFile))
   .parse(process.argv);
