64f5566690949db760d16eea4b5a9659c3dba5fc
jeshicawang <jeshicawang@gmail.com>
2017-02-16 20:04:01 -0800

    Updating versioning. #17

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,7 +4,7 @@ const Envision = require('../envision.js');
 const program = require('commander');
 
 program
-  .version('1.0.7')
+  .version('1.0.8')
   .arguments('<root-file>')
   .action((rootFile) => Envision.parse(rootFile))
   .parse(process.argv);
