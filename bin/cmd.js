1242031c7fdc4eef20c16c40aa23b802b2a15919
jeshicawang <jeshicawang@gmail.com>
2017-02-16 19:59:15 -0800

    Modifying CLI version. #17

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,7 +4,7 @@ const Envision = require('../envision.js');
 const program = require('commander');
 
 program
-  .version('1.0.6')
+  .version('1.0.7')
   .arguments('<root-file>')
   .action((rootFile) => Envision.parse(rootFile))
   .parse(process.argv);
